4fd1ea55ff20393f29d649f0dd485e3e902ee3cb
AndNab <149181495+AndNab@users.noreply.github.com>
2023-10-28 13:55:38 +0200

    Update __init__.py - added hook for gcode; renamed to MouseMonitor

--- octoprint_spool_sensor/__init__.py
+++ octoprint_spool_sensor/__init__.py
@@ -178,13 +178,14 @@ class SpoolSensorPlugin(octoprint.plugin.StartupPlugin,
         thread = Thread(target=self.start_mouse_movement_listener)
         thread.start()
 
-__plugin_name__ = "Spool Sensor"
-__plugin_version__ = "1.0.3"
+__plugin_name__ = "MouseMonitor"
+__plugin_version__ = "1.0.4"
 __plugin_pythoncompat__ = ">=2.7,<4"
-__plugin_implementation__ = SpoolSensorPlugin()
+__plugin_implementation__ = MouseMonitorPlugin()
 
 def __plugin_load__():
     global __plugin_hooks__
     __plugin_hooks__ = {
-        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+        "octoprint.comm.protocol.gcode.sent": __plugin_implementation__.distance_detection
 }
