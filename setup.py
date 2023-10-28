427a0529491344810bc7211eec7a57d16e8c0148
AndNab <149181495+AndNab@users.noreply.github.com>
2023-10-28 13:59:03 +0200

    Update setup.py - renaming

--- setup.py
+++ setup.py
@@ -4,30 +4,30 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "spool_sensor"
+plugin_identifier = "MouseMonitor"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_spool_sensor"
+plugin_package = "octoprint_MouseMonitor"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-Spool-Sensor"
+plugin_name = "OctoPrint-MouseMonitor"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.0.3"
+plugin_version = "1.0.4"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """A filament spool movement monitor which pauses the print job when no movement is detected, letting you resume it."""
+plugin_description = """A filament monitor based on mouse input"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "OutsourcedGuru"
+plugin_author = "tbd"
 
 # The plugin's author's mail address.
-plugin_author_email = "support@outsourced.guru"
+plugin_author_email = "tbd"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/OutsourcedGuru/Octoprint-Spool-Sensor"
+plugin_url = "https://github.com/"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "GPLv3"
